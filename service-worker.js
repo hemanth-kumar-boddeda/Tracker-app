23beed41fe6c0b5530a34366ba38ae15e794d9b0
HEMANTH KUMAR BODDEDA <82110100+hemanth-kumar-boddeda@users.noreply.github.com>
2026-03-31 01:34:00 +0530

    Add service worker for caching strategy to enable offline functionality

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+// service-worker.js
+
+const CACHE_NAME = 'app-cache-v1';
+const urlsToCache = [
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Caching app shell');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Cached files available, return them
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
